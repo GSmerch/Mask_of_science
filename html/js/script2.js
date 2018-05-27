89f9fd08d7529c3f1aefff5285c293ccd42bded9
GSmerch <39675570+GSmerch@users.noreply.github.com>
2018-05-27 19:30:56 +0300

    Smerch commiy

---
+++ html/js/script2.js
@@ -0,0 +1,36 @@
+vv.onclick = function myFunction(){
+	var x = document.getElementById("vvi");
+
+	if(x.className === "vvc"){
+		x.className += " rk";
+	} else{
+		x.className = "vvc";
+	}
+}
+gl1.onclick = function myFunction(){
+	var x = document.getElementById("gl1i");
+
+	if(x.className === "gl1c"){
+		x.className += " rk";
+	} else{
+		x.className = "gl1c";
+	}
+}
+gl2.onclick = function myFunction(){
+	var x = document.getElementById("gl2i");
+
+	if(x.className === "gl2c"){
+		x.className += " rk";
+	} else{
+		x.className = "gl2c";
+	}
+}
+gl3.onclick = function myFunction(){
+	var x = document.getElementById("gl3i");
+
+	if(x.className === "gl3c"){
+		x.className += " rk";
+	} else{
+		x.className = "gl3c";
+	}
+}
